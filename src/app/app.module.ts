e01c0268aabe39a394c971d460485e40ed1aa65d
david04davidgr <davidgarcia.r.04@gmail.com>
2025-01-19 12:52:39 +0100

    Add DBZ module and main page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { CounterModule } from './counter/components/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
+
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
